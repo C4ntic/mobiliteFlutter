2d4000d478271d5cf8fa3dd73f35e5b4f132b813
SachDn <72878858+C4ntic@users.noreply.github.com>
2022-11-28 11:41:13 +0100

    searchBar

--- lib/search.dart
+++ lib/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'controller/Controller.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -10,6 +11,7 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   Icon customIcon = const Icon(Icons.search);
   Widget customSearchBar = const Text('Recherche');
+  String value = '';
 
   @override
   void initState() {
@@ -22,105 +24,37 @@ class _SearchState extends State<Search> {
       appBar: AppBar(
         title: const Text('Recherche'),
         backgroundColor: Colors.black,
-        actions: [
-          IconButton(
-            onPressed: () {
-              // method to show the search bar
-              showSearch(
-                  context: context,
-                  // delegate to customize the search bar
-                  delegate: CustomSearchDelegate()
-              );
-            },
-            icon: const Icon(Icons.search),
-          )
-        ],
       ),
       backgroundColor: const Color(0xFF2C393F),
       body: Container(
-          alignment: Alignment.topCenter,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
-              ])),
-    );
-  }
-
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  // Demo list to show querying
-  List<String> searchTerms = [
-    "Le Parrain",
-    "Breaking Bad",
-    "Game of Thrones",
-    "Peaky Blinders"
-  ];
-
-  // first overwrite to
-  // clear the search text
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: Icon(Icons.clear),
-      ),
-    ];
-  }
-
-  // second overwrite to pop out of search menu
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  // third overwrite to show query result
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  // last overwrite to show the
-  // querying process at the runtime
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
+        alignment: Alignment.topCenter,
+        child: TextField(
+          onSubmitted: (String value) async {
+            print("$value");
+            this.value = "$value";
+            print('la valeur de nahelbok est : ' + this.value);
+            // Navigator.pop();
+          },
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none
+              ),
+              hintText: 'Search',
+              hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18
+              ),
+              prefixIcon: Container(
+                padding: EdgeInsets.all(15),
+                child: Image.asset('assets/icons/search.png'),
+                width: 18,
+              )
+          ),
+        ),),
     );
   }
 }
